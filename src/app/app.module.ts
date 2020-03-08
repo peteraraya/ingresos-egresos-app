49bee61b90ffc551cc403f028054698817d2c1b3
Peter <piteraraya@gmail.com>
2020-03-08 01:45:11 -0300

    Primer commit de app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// Rutas
+import { AppRoutingModule } from './app.routing.module';
+// Módulos
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
+import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IngresoEgresoComponent,
+    EstadisticaComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    DetalleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Rutas
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
